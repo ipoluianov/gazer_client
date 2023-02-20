66d6cd8a497c36d5a104ef2de15bb2a12194aecd
Ivan Poluianov <ivan.poluianov@gmail.com>
2023-02-20 22:52:37 +0100

    Update unit_edit_form.dart

--- lib/forms/units/unit_edit/unit_edit_form.dart
+++ lib/forms/units/unit_edit/unit_edit_form.dart
@@ -26,50 +26,33 @@ class UnitEditForm extends StatefulWidget {
 }
 
 class UnitEditFormSt extends State<UnitEditForm> {
-  late UnitEditFormCubit cubit;
-
   late String name = "";
   bool nameLoadedByUnitType = false;
-  late Map<String, dynamic> currentConfig;
+  Map<String, dynamic> currentConfig = <String, dynamic>{};
   late List<dynamic> currentConfigMeta;
   //late String conf = "";
 
-  late Future<UnitTypeConfigMetaResponse> _futureUnitTypeConfigMeta;
-  late Future<UnitGetConfigResponse> _futureUnitGetConfig;
+  //late Future<UnitTypeConfigMetaResponse> _futureUnitTypeConfigMeta;
+  //late Future<UnitGetConfigResponse> _futureUnitGetConfig;
   //final ScrollController _scrollController = ScrollController();
   final ScrollController _scrollController1 = ScrollController();
   //final ScrollController _scrollController2 = ScrollController();
   final TextEditingController _txtNameController = TextEditingController();
 
+  UnitTypeConfigMetaResponse? unitConfigMeta_;
+  UnitGetConfigResponse? unitConfig_;
+
   @override
   void initState() {
-    if (widget.arg.unitId == "") {
-      currentConfig = <String, dynamic>{};
-      _futureUnitTypeConfigMeta = Repository()
-          .client(widget.arg.connection)
-          .unitTypeConfigMeta(widget.arg.unitType);
-    } else {
-      _futureUnitGetConfig = Repository()
-          .client(widget.arg.connection)
-          .unitsGetConfig(widget.arg.unitId);
-      _futureUnitGetConfig.catchError(() {}); // TODO: error
-    }
-
-    Repository()
-        .client(widget.arg.connection)
-        .unitsGetConfig(widget.arg.unitId)
-        .then((value) {
-      name = value.unitName;
-      _txtNameController.text = value.unitName;
-    }).catchError(() {
-      // TODO: error
-    });
-
     super.initState();
 
-    cubit = UnitEditFormCubit(UnitEditFormStateLoading());
-
-    cubit.load(widget.arg.connection);
+    if (widget.arg.unitId == "") {
+      loadUnitConfigMeta();
+    } else {
+      if (widget.arg.unitType != "") {
+        loadUnitConfig();
+      }
+    }
   }
 
   @override
@@ -80,93 +63,106 @@ class UnitEditFormSt extends State<UnitEditForm> {
     super.dispose();
   }
 
+  void loadUnitConfigMeta() {
+    Repository()
+        .client(widget.arg.connection)
+        .unitTypeConfigMeta(widget.arg.unitType)
+        .then((value) {
+      if (mounted) {
+        setState(() {
+          unitConfigMeta_ = value;
+        });
+      }
+    }).catchError((err) {
+      print(err);
+    });
+  }
+
+  void loadUnitConfig() {
+    Repository()
+        .client(widget.arg.connection)
+        .unitsGetConfig(widget.arg.unitId)
+        .then((value) {
+      name = value.unitName;
+      _txtNameController.text = value.unitName;
+    }).catchError((err) {
+      print("unit config loading $err");
+    });
+  }
+
   // UnitTypeConfigMetaResponse
 
   Widget buildNewUnitConfig() {
-    return FutureBuilder<UnitTypeConfigMetaResponse>(
-      future: _futureUnitTypeConfigMeta,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          currentConfigMeta = jsonDecode(snapshot.data!.unitTypeConfigMeta);
-          if (name == "" && !nameLoadedByUnitType) {
-            _txtNameController.text = snapshot.data!.unitType;
-            name = _txtNameController.text;
-            nameLoadedByUnitType = true;
-          }
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              //buildToolbar(),
-              Container(
-                child: TextField(
-                  controller: _txtNameController,
-                  decoration: const InputDecoration(
-                    hintText: "Name",
-                    labelText: "Name",
-                  ),
-                  onChanged: (text) {
-                    setState(() {
-                      name = text;
-                    });
-                  },
-                ),
+    if (unitConfigMeta_ != null) {
+      currentConfigMeta = jsonDecode(unitConfigMeta_!.unitTypeConfigMeta);
+      if (name == "" && !nameLoadedByUnitType) {
+        _txtNameController.text = unitConfigMeta_!.unitType;
+        name = _txtNameController.text;
+        nameLoadedByUnitType = true;
+      }
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //buildToolbar(),
+          Container(
+            child: TextField(
+              controller: _txtNameController,
+              decoration: const InputDecoration(
+                hintText: "Name",
+                labelText: "Name",
               ),
-              UnitConfigObject(Repository().client(widget.arg.connection),
-                  currentConfigMeta, currentConfig, () {
-                setState(() {});
-              }),
-            ],
-          );
-        } else if (snapshot.hasError) {
-          return const Text("Error");
-        }
-        return Container();
-      },
-    );
+              onChanged: (text) {
+                setState(() {
+                  name = text;
+                });
+              },
+            ),
+          ),
+          UnitConfigObject(Repository().client(widget.arg.connection),
+              currentConfigMeta, currentConfig, () {
+            setState(() {});
+          }),
+        ],
+      );
+    }
+    return Container();
   }
 
   Widget buildEditUnitConfig() {
-    return FutureBuilder<UnitGetConfigResponse>(
-      future: _futureUnitGetConfig,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          try {
-            currentConfig = jsonDecode(snapshot.data!.unitConfig);
-          } catch (ex) {
-            currentConfig = <String, dynamic>{};
-          }
+    if (unitConfig_ != null) {
+      try {
+        currentConfig = jsonDecode(unitConfig_!.unitConfig);
+      } catch (ex) {
+        currentConfig = <String, dynamic>{};
+      }
 
-          currentConfigMeta = jsonDecode(snapshot.data!.unitConfigMeta);
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              //buildToolbar(),
-              Container(
-                child: TextField(
-                  controller: _txtNameController,
-                  decoration: const InputDecoration(
-                    hintText: "Name",
-                    labelText: "Name",
-                  ),
-                  onChanged: (text) {
-                    setState(() {
-                      name = text;
-                    });
-                  },
-                ),
+      currentConfigMeta = jsonDecode(unitConfig_!.unitConfigMeta);
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //buildToolbar(),
+          Container(
+            child: TextField(
+              controller: _txtNameController,
+              decoration: const InputDecoration(
+                hintText: "Name",
+                labelText: "Name",
               ),
-              UnitConfigObject(Repository().client(widget.arg.connection),
-                  currentConfigMeta, currentConfig, () {
-                setState(() {});
-              }),
-            ],
-          );
-        } else if (snapshot.hasError) {
-          return const Text("Error");
-        }
-        return Container();
-      },
-    );
+              onChanged: (text) {
+                setState(() {
+                  name = text;
+                });
+              },
+            ),
+          ),
+          UnitConfigObject(Repository().client(widget.arg.connection),
+              currentConfigMeta, currentConfig, () {
+            setState(() {});
+          }),
+        ],
+      );
+    }
+    return Container();
   }
 
   Widget buildConfigWidget() {
